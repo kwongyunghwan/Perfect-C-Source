8f1b0ddc470fd4fbbe4e849178b825933c545a8a
kwongyunghwan <roope97@naver.com>
2016-10-03 20:50:32 +0900

    Add files via upload

---
+++ twobitop.c
@@ -0,0 +1,22 @@
+// file: twobitop.c
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 0, b = 0;
+
+	printf("비트 연산이 가능한 두 정수를 입력하세요\n");
+	scanf("%d %d", &a, &b);
+
+	printf("%3d & %3d ==> %3d\n", a, b, a & b);
+	printf("%3d | %3d ==> %3d\n", a, b, a | b);
+	printf("%3d ^ %3d ==> %3d\n", a, b, a ^ b);
+	printf(" ~%3d ==>  %3d\n", a, ~a);
+	printf("%3d >> %3d ==> %3d\n", a, b, a >> b);
+	printf("%3d << %3d ==> %3d\n", a, b, a << b);
+
+
+	return 0;
+}
